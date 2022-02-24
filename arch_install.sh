97a7b8fa9a64d1d5c1b684a2388e358250c35a92
Denys Dovzhnyi <dovzhnyi.denys@pm.me>
2022-02-24 20:52:22 +0200

    v 0.9.0. Added option to install KDE Plasma and Network Manager.

--- arch_install.sh
+++ arch_install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+while [ -n "$1" ]; do
+  case "$1" in 
+    -P|--plasma-desktop) plasma_desktop=1; shift 1;;
+  esac
+done
+
 loadkeys us
 timedatectl set-ntp true
 pacstrap /mnt base linux-lts linux-firmware dhcpcd iwd vim screen grub efibootmgr archlinux-keyring dhcpcd
+[[ -n "$plasma_desktop" ]] && pacstrap /mnt xorg plasma-meta plasma-nm konsole dolphin networkmanager networkmanager-l2tp
 grub_id=$(mount | grep /mnt | cut -d' ' -f1 | rev | cut -d/ -f1 | rev | cut -d'-' -f1)
 genfstab -U /mnt >> /mnt/etc/fstab
 cat << EOF > /chroot_template.sh
@@ -18,7 +25,6 @@ sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cryptdevice=UUID=part_uuid:p
 sed -i -e s/#GRUB_DISABLE_SUBMENU=y/GRUB_DISABLE_SUBMENU=y/ -e s/GRUB_DEFAULT=0s/GRUB_DEFAULT=saved/ -e s/#GRUB_SAVEDEFAULT=true/GRUB_SAVEDEFAULT=true/ /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable dhcpcd
-passwd
 EOF
 if [[ -z "$(mount | grep /mnt | grep mapper)" ]]; then
   sed -i /mkinitcpio/d /chroot_template.sh
@@ -30,6 +36,10 @@ else
 fi 
 [[ ! -d /sys/firmware/efi/efivars ]] && sed -i "s:grub-install --target=x86_64-efi --efi-directory=\/efi:grub-install --target=i386-pc \$\(mount | grep \"on \/ \" | cut -d\' \' -f1\):" /chroot_template.sh 
 sed -i "s:--bootloader-id=GRUB:--bootloader-id=Arch-${grub_id}:" /chroot_template.sh
+
+[[ -n "$plasma_desktop" ]] && echo 'systemctl enable sddb && systemctl enable NetworkManager' >> /chroot_template.sh
+
+echo passwd >> /chroot_template.sh
 mv -v /chroot_template.sh /mnt/chroot_part.sh
 chmod 700 /mnt/chroot_part.sh
 arch-chroot /mnt ./chroot_part.sh
